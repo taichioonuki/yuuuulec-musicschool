8bf425cec2a152f7522d00f8cd6be1de3d35fce9
’yuuulec’ <sprouse.yuuka@gmail.com>
2026-01-23 21:54:16 +0900

    修正

--- header.php
+++ header.php
@@ -10,7 +10,8 @@
 <body style="display: none;">
     <header id="header" class="header">
         <div class="header__inner inner">
-            <?php $tag = is_front_page() ? 'h1' : 'div'; ?>
+            <?php ( is_front_page() || is_search() ) ? 'h1' : 'div'; 
+?>
             <<?php echo $tag; ?> class="header__logo">
                 <a href="<?php echo esc_url(home_url('/')); ?>">
                     <span class="header__logo-img">
